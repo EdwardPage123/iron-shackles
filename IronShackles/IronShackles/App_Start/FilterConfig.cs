a0dae62cb5899dbfe2ad31c1252da55f65e300a5
Edward Page <yasnayo@inbox.lv>
2018-12-25 01:58:39 +0200

    Pamata projekts

---
+++ IronShackles/IronShackles/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace IronShackles
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
